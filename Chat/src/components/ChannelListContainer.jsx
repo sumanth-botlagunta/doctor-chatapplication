c455eddab3841a534b2ca9c78113887b9e141f6d
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2023-03-02 00:17:58 +0530

    channel search

--- Chat/src/components/ChannelListContainer.jsx
+++ Chat/src/components/ChannelListContainer.jsx
@@ -36,6 +36,7 @@ const ChannelListContainer = () => {
       <SideBar />
       <div className="channel-list__list__wrapper">
         <CompanyHeader />
+        <ChannelSearch />
       </div>
     </>
   );
